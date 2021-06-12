b086899ada9e434aa2bebb518baa01a9a1967540
Sushil Tiwari <stiwary.1985@gmail.com>
2021-06-12 19:08:10 +0530

    Angular Project Addess

---
+++ NegotiationApp/WebApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
